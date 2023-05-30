ad3f6e3c3d682a4e391647c4cbdb2f0779637c81
MarioCSalgado <mario_cso15@hotmail.com>
2023-05-30 18:20:17 -0400

    Subida codigo Tarea1 con Maven

---
+++ Tarea3-2023/src/main/java/com/mycompany/tarea3/NoHayProductoException.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.tarea3;
+
+/**
+*clase de excepcion en caso de que no haya productos
+*/
+public class NoHayProductoException extends Exception{
+	/**
+	*Metodo constructor de la NoHayProductoException
+	*@param texto Es un texto que entrega para notificar la devolución de moneda.
+	*/
+    public NoHayProductoException(String texto){
+        super(texto);
+    }
+} 
---
+++ Tarea3-2023/src/main/java/com/mycompany/tarea3/PagoIncorrectoException.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.tarea3;
+/**
+*clase de excepcion en caso de e quiere comprar un producto por un valor inferior al precio
+*/
+public class PagoIncorrectoException extends Exception {
+	/**
+	*Metodo constructor de PagoIncorrectoException, devuelve un texto indicando la devolucion de la moneda como vuelto
+	*/
+    public PagoIncorrectoException(String texto) {
+        super(texto);
+    }
+}
